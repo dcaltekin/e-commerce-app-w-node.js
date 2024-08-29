c4037c9eb4fc669d2756065c3a6cd0bd878d78c7
dcaltekin <dcaltekin@gmail.com>
2024-08-29 11:32:30 +0300

    ADDED: loading-for-top-selling-products

--- frontend/components/dashboard/TopSelling.jsx
+++ frontend/components/dashboard/TopSelling.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export default function TopSellingProducts() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchTopSellingProducts = async () => {
@@ -13,16 +14,26 @@ export default function TopSellingProducts() {
         setProducts(response.data);
       } catch (error) {
         console.error("Error fetching top-selling products:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchTopSellingProducts();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="text-center text-gray-500 mt-8">
+        <p>Yükleniyor...</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">En Çok Satan Ürünler (Top 10)</h1>
-      <div className="grid  gap-4 mt-8">
+      <div className="grid gap-4 mt-8">
         {products.length > 0 ? (
           products.map((product) => (
             <div
